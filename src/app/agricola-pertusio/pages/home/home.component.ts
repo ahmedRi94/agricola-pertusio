aa49a7119ac5204e1569333b910703e646c6f2d8
Ahmed Riahi <amedin94@live.it>
2023-08-08 18:28:43 +0200

    fixed home page

--- src/app/agricola-pertusio/pages/home/home.component.ts
+++ src/app/agricola-pertusio/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import configJson from "./config/home.json"
 @Component( {
   selector: 'app-home',
@@ -7,8 +7,34 @@ import configJson from "./config/home.json"
 } )
 export class HomeComponent {
   content: any;
+  sizeWindow: number;
 
   constructor() {
     this.content = configJson
+    this.sizeWindow = window.innerWidth
+  }
+
+  getCorrectlyCol( isFirst: boolean ) {
+    switch ( isFirst ) {
+      case true: {
+        if ( this.sizeWindow > 1400 )
+          return '-8'
+        if ( this.sizeWindow > 1200 )
+          return '-7'
+        if ( this.sizeWindow > 992 )
+          return '-6'
+        return null
+      }
+      default:
+        if ( this.sizeWindow < 992 )
+          return ' text-center'
+        return null
+    }
+  }
+
+
+  @HostListener( 'window:resize', ['$event'] )
+  onResize( event: any ) {
+    this.sizeWindow = event.target.innerWidth
   }
 }
